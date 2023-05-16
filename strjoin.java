f07f7c588ca025e926ab1016c05cc18c9fd90d90
Harshit Chandra <harshitchndr@gmail.com>
2023-05-16 17:11:05 +0530

    program to join two strings

---
+++ strjoin.java
@@ -0,0 +1,8 @@
+public class Stringjoin {
+    public static void main(String[] args) {
+        String str1 = "Hello, ";
+        String str2 = "How are you?";
+        String joinedStr = str1.concat(str2);
+        System.out.println("Joined string is: " + joinedStr);
+    }
+}
